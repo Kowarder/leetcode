e12dec3ab4e270bdb36304b484a95934752c8009
Kowarder <70952974+Kowarder@users.noreply.github.com>
2024-01-06 23:55:27 -0500

    Update 167.py

--- 167.py
+++ 167.py
@@ -1,6 +1,12 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        for i in range(len(numbers)-1):
-            for j in range(i+1, len(numbers)):
-                if numbers[i] + numbers[j] == target:
-                    return [i+1, j+1]
+        i, j = 0, len(numbers)-1
+        while i < j:
+            s = numbers[i]+numbers[j]
+            if s == target:
+                return [i+1, j+1]
+            if s > target:
+                j -= 1
+            else:
+                i += 1
+        return []
